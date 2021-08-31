a7f2934cb3eb0ebb87cae607315257b572362d45
Mahitha Gurrala <59527081+mahitha2001@users.noreply.github.com>
2021-09-01 01:23:38 +0530

    Add files via upload

---
+++ candlestick.js
@@ -0,0 +1,57 @@
+var chart = LightweightCharts.createChart(document.body, {
+	width: 600,
+  height: 300,
+	layout: {
+		backgroundColor: '#fff',
+		textColor: 'rgba(255, 255, 255, 0.9)',
+	},
+	grid: {
+		vertLines: {
+			color: 'rgba(197, 203, 206, 0.5)',
+		},
+		horzLines: {
+			color: 'rgba(197, 203, 206, 0.5)',
+		},
+	},
+	crosshair: {
+		mode: LightweightCharts.CrosshairMode.Normal,
+	},
+	rightPriceScale: {
+		borderColor: 'rgba(197, 203, 206, 0.8)',
+	},
+	timeScale: {
+		borderColor: 'rgba(197, 203, 206, 0.8)',
+	},
+});
+
+var candleSeries = chart.addCandlestickSeries({
+  upColor: 'rgba(0, 255, 0, 1)',
+  downColor: '#ff0000',
+  borderDownColor: 'rgba(255, 0, 0, 1)',
+  borderUpColor: 'rgba(0, 255, 0, 1)',
+  wickDownColor: 'rgba(255, 0, 0, 1)',
+  wickUpColor: 'rgba(0, 255, 0, 1)',
+});
+fetch('Stock List.json').then(response => response.json())
+.then(result => {
+  
+  var test=result.filter(d => {
+    console.log(d.key);
+    if(d.key=='AAPL'){
+      console.log("true");
+      return true;
+    }
+    else{
+      console.log("false");
+      return false;
+    }
+  })
+  console.log(test);
+  const cdata=test.map(d => {
+    console.log(d.date);
+    return {time:d.date, open: d.open, high: d.high, low: d.low, close: d.close};
+  })
+  candleSeries.setData(cdata);
+}).catch(err => {
+  console.log(err);
+})
